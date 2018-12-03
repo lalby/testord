c278af24b73870e5166aed9e037c94a1de69242c
lalby <lalby2007@gmail.com>
2018-12-03 12:24:56 -0800

    rev2

---
+++ utility.h
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include <sstream>
+#include<string>
+
+
+void split(const std::string &s,std::vector<char *> &tokens,const char delim=' ')
+{
+    std::stringstream ss(s);
+    std::string token;
+    while(getline(ss,token,delim))
+    {
+	char *temp=new char(token.size()+1);
+	strncpy(temp,token.c_str(),token.length()+1);
+	tokens.push_back(temp);
+    }
+
+}		
+	
+
